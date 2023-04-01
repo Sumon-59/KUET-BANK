f13ed048ce73f3f7bcb2e688f5fd66844b94b7b3
Sumon-59 <hossain1907109@stud.kuet.ac.bd>
2023-04-02 01:23:35 +0600

    getter and setter updated

---
+++ Getter Setter.java
@@ -0,0 +1,21 @@
+public class GetterSetterExample   
+{  
+    public salary;  
+      
+    public storeSalaryDB(int salary)  
+    {  
+        // code for storing the salary in the database  
+    }  
+      
+    // main method  
+    public static void main(String argvs[])   
+    {  
+      GetterSetterExample obj = new GetterSetterExample();      
+        
+      obj.salary = -50000;  
+        
+      // storing salary in database  
+      obj.storeSalaryDB(salary);  
+  
+    }  
+}  
